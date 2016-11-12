541f2d3910e1118839c40989e3425bda34651a5d
Pat Hackman <pajohack@gmail.com>
2016-11-12 19:54:30 +0000

    Create Users Section Working Completely

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -20,8 +20,8 @@
       {!! Form::select('is_active', array(1 => 'Active', 0 => 'Not Active'), 0, ['class'=>'form-control']) !!}
     </div>
     <div class="form-group">
-      {!! Form::label('file', 'Title:') !!}
-      {!! Form::file('file', null, ['class'=>'form-control']) !!}
+      {!! Form::label('photo_id', 'Photopic:') !!}
+      {!! Form::file('photo_id', null, ['class'=>'form-control']) !!}
     </div>
     <div class="form-group">
       {!! Form::label('password', 'Password:') !!}
